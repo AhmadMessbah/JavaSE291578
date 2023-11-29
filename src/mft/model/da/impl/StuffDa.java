70e99d4af1a3b6d5147fc2c3314c9031745aed06
aryan_drv <aryan.darvishzadeh@gmail.com>
2023-11-29 08:55:05 +0330

    creat StuddDa class

---
+++ src/mft/model/da/impl/StuffDa.java
@@ -0,0 +1,32 @@
+package mft.model.da.impl;
+
+import mft.model.entity.Stuff;
+
+import java.util.List;
+
+public class StuffDa implements Da<Stuff>{
+    @Override
+    public Stuff save(Stuff stuff) throws Exception {
+        return null;
+    }
+
+    @Override
+    public Stuff edit(Stuff stuff) throws Exception {
+        return null;
+    }
+
+    @Override
+    public Stuff remove(int id) throws Exception {
+        return null;
+    }
+
+    @Override
+    public List<Stuff> findAll() throws Exception {
+        return null;
+    }
+
+    @Override
+    public Stuff findById(int id) throws Exception {
+        return null;
+    }
+}
