4d8ec2355d981afbef7175276d2d1f92419709c5
Aryan <Aryan@gmail.com>
2023-12-02 22:51:55 +0330

    nothing

---
+++ src/mft/controller/StuffController.java
@@ -0,0 +1,28 @@
+package mft.controller;
+
+import mft.model.da.PersonDa;
+import mft.model.da.StuffDa;
+import mft.model.entity.Person;
+import mft.model.entity.Stuff;
+
+import java.util.regex.Pattern;
+
+public class StuffController {
+    public static Stuff save(String name, String brand, String groupTitle) {
+        try {
+            if (Pattern.matches("^[a-zA-Z\\s]{3,30}$", name) &&
+                    Pattern.matches("^[a-zA-Z\\s]{3,30}$", brand) &&
+                    Pattern.matches("^[a-zA-Z\\s]{3,30}$", groupTitle)) {
+                Stuff stuff = Stuff.builder().name(name).brand(brand).groupTitle(groupTitle).build();
+
+                StuffDa stuffDa = new StuffDa();
+                stuffDa.save(stuff);
+                return stuff;
+            }
+        } catch (Exception e) {
+            System.out.println("Error : " + e.getMessage());
+        }
+        return null;
+    }
+
+}
