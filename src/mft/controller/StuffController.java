805f890deef0fbac5267bbdb2cf93f75d39fd150
Aryan <Aryan@gmail.com>
2023-12-02 22:54:58 +0330

    nothing

--- src/mft/controller/StuffController.java
+++ src/mft/controller/StuffController.java
@@ -25,4 +25,21 @@ public class StuffController {
         return null;
     }
 
+    public static Stuff edit(String name, String brand, String groupTitle) {
+        try {
+            if (Pattern.matches("^[a-zA-Z\\s]{3,30}$", name) &&
+                    Pattern.matches("^[a-zA-Z\\s]{3,30}$", brand) &&
+                    Pattern.matches("^[a-zA-Z\\s]{3,30}$", groupTitle)) {
+                Stuff stuff = Stuff.builder().name(name).brand(brand).groupTitle(groupTitle).build();
+
+                StuffDa stuffDa = new StuffDa();
+                stuffDa.save(stuff);
+                return stuff;
+            }
+        } catch (Exception e) {
+            System.out.println("Error : " + e.getMessage());
+        }
+        return null;
+    }
+
 }
