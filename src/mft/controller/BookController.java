e0fe1370ac4b7225ba5325f1328f52a4b2cc8ea7
yousefpoue 1983 <miss.yousefpour@gmail.com>
2023-12-10 17:37:09 +0330

    yousefpour.5

--- src/mft/controller/BookController.java
+++ src/mft/controller/BookController.java
@@ -9,39 +9,37 @@ import java.util.regex.Pattern;
 
 public class BookController {
 
-        public static Book save(String title, String author) {
-            try {
-                if ( Book book;
-                Pattern.matches("^[a-zA-Z\\s]{3,30}$", title) &&
-                        Pattern.matches("^[a-zA-Z\\s]{3,30}$", author) && {
-                        Book book = Book.builder().title(title).author(author).build();
+    public static Book save(String title, String author) {
+        try {
+            if (Pattern.matches("^[a-zA-Z\\s]{3,30}$", title) &&
+                    Pattern.matches("^[a-zA-Z\\s]{3,30}$", author)) {
+                Book book = Book.builder().title(title).author(author).build();
 
-                    BookDa bookDa = new bookDa();
-                    bookDa.save(Book);
-                    return book;
-                }
-            } catch (Exception e) {
-                System.out.println("Error : " + e.getMessage());
+                BookDa bookDa = new BookDa();
+                bookDa.save(book);
+                return book;
             }
-            return null;
+        } catch (Exception e) {
+            System.out.println("Error : " + e.getMessage());
         }
+        return null;
+    }
 
-        public static Book edit(String title, String author) {
-            try {
-                if (Pattern.matches("^[a-zA-Z\\s]{3,30}$", title) &&
-                        Pattern.matches("^[a-zA-Z\\s]{3,30}$", author) &&)) {
-                    Book book = Book.builder().title(title).author(author).build();
+    public static Book edit(String title, String author) {
+        try {
+            if (Pattern.matches("^[a-zA-Z\\s]{3,30}$", title) &&
+                    Pattern.matches("^[a-zA-Z\\s]{3,30}$", author) &&)){
+                Book book = Book.builder().title(title).author(author).build();
 
-                    BookDa bookDa = new BookDa();
-                    bookDa.save(book);
-                    return book;
-                }
-            } catch (Exception e) {
-                System.out.println("Error : " + e.getMessage());
+                BookDa bookDa = new BookDa();
+                bookDa.save(book);
+                return book;
             }
-            return null;
+        } catch (Exception e) {
+            System.out.println("Error : " + e.getMessage());
         }
-
+        return null;
+    }
 
 
 }
