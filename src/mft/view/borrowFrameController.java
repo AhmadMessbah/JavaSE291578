08c019512c361a6d5c9fbf8d034737c849fcb32a
Haghighi.F <f.haghighi98@gmail.com>
2023-12-19 16:01:02 +0330

    haghighi-f repeat code borrowFrameController

--- src/mft/view/borrowFrameController.java
+++ src/mft/view/borrowFrameController.java
@@ -18,54 +18,46 @@ import java.util.ResourceBundle;
 
 public class borrowFrameController implements Initializable {
     @FXML
-    private Button saveBtn,editBtn,removeBtn;
+    private Button saveBtn, editBtn, removeBtn;
 
     @FXML
-    private TextField personTxt,bookTxt;
+    private TextField personTxt, bookTxt;
 
     @FXML
-    private DatePicker borrowDate,returnDate;
+    private DatePicker borrowDate, returnDate;
 
     @FXML
     private TableView<Borrow> borrowTbl;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        saveBtn.setOnAction(event -> {});
-        editBtn.setOnAction(event -> {});
-        removeBtn.setOnAction(event -> {});
+        saveBtn.setOnAction(event -> {
+        });
+        editBtn.setOnAction(event -> {
+        });
+        removeBtn.setOnAction(event -> {
+        });
     }
 
-    public void showDataBorrow(List<Borrow> borrowList){
+    public void showDataBorrow(List<Borrow> borrowList) {
         ObservableList<Borrow> borrows = FXCollections.observableList(borrowList);
 
-        TableColumn<Borrow , Integer> idcol = new TableColumn<>("Id");
+        TableColumn<Borrow, Integer> idcol = new TableColumn<>("Id");
         idcol.setCellValueFactory(new PropertyValueFactory<>("id"));
 
-        TableColumn<Borrow , Person> personCol = new TableColumn<>("Person");
+        TableColumn<Borrow, Person> personCol = new TableColumn<>("Person");
         idcol.setCellValueFactory(new PropertyValueFactory<>("person"));
 
-        TableColumn<Borrow , Book> bookCol = new TableColumn<>("Book");
+        TableColumn<Borrow, Book> bookCol = new TableColumn<>("Book");
         idcol.setCellValueFactory(new PropertyValueFactory<>("book"));
 
-        TableColumn<Borrow , Date> borrowDateCol = new TableColumn<>("Borrow Date");
+        TableColumn<Borrow, Date> borrowDateCol = new TableColumn<>("Borrow Date");
         idcol.setCellValueFactory(new PropertyValueFactory<>("borrowTimeStamp"));
 
-        TableColumn<Borrow , Date> returnDateCol = new TableColumn<>("Return Date");
+        TableColumn<Borrow, Date> returnDateCol = new TableColumn<>("Return Date");
         idcol.setCellValueFactory(new PropertyValueFactory<>("returnTimeStamp"));
 
-        borrowTbl.getColumns().addAll(idcol,personCol,bookCol,borrowDateCol,returnDateCol);
+        borrowTbl.getColumns().addAll(idcol, personCol, bookCol, borrowDateCol, returnDateCol);
         borrowTbl.setItems(borrows);
-=======
-import javafx.fxml.Initializable;
-
-import java.net.URL;
-import java.util.ResourceBundle;
-
-public class borrowFrameController implements Initializable {
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
-
->>>>>>> origin/master
     }
 }
