d209bffbb7909f9e00e8810ce0a22578f6635655
Haghighi.F <f.haghighi98@gmail.com>
2023-12-19 15:49:47 +0330

    haghighi-f change borrowFrameController

---
+++ src/mft/view/borrowFrameController.java
@@ -0,0 +1,13 @@
+package mft.view;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class borrowFrameController implements Initializable {
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
